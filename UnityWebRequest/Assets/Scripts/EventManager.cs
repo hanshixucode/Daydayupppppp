f4f038268634fd654ef3b368fd5d053f3e7a9347
hanshixucode <13569270593@163.com>
2021-09-16 23:12:04 +0800

    EventDispatch

--- UnityWebRequest/Assets/Scripts/EventManager.cs
+++ UnityWebRequest/Assets/Scripts/EventManager.cs
@@ -20,16 +20,41 @@ public class EventDispatch : Single<EventDispatch>
 
     public void AddEvent(string name,Action ac)
     {
-        
+        Delegate Ac;
+        if (Events.TryGetValue(name, out Ac))
+        {
+            
+        }
+        else
+        {
+            Events.Add(name,ac);
+        }
     }
     
     public void RemoveEvent(string name,Action ac)
     {
-        
+        if(_event == null) return;
+        Delegate Ac;
+        if (_event.TryGetValue(name, out Ac))
+        {
+            _event.Remove(name);
+        }
     }
     
-    public void TriggerEvent(string name,Action ac)
+    public void TriggerEvent(string name)
     {
-        
+        if(_event == null) return;
+        Delegate Ac;
+        if (_event.TryGetValue(name, out Ac))
+        {
+            Delegate[] aclist = Ac.GetInvocationList();
+            for (int i = 0; i < aclist.Length; i++)
+            {
+                Action _ac = aclist[i] as Action;
+                if (_ac != null)
+                    _ac();
+            }
+            
+        }
     }
 }
---
+++ UnityWebRequest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        EventDispatch.Instance.AddEvent("enterbutton",OnclickButton);
+    }
+
+    private void OnDisable()
+    {
+        EventDispatch.Instance.RemoveEvent("enterbutton",OnclickButton);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnclickButtonFunc()
+    {
+        EventDispatch.Instance.TriggerEvent("enterbutton");
+    }
+    private void OnclickButton()
+    {
+        Debug.Log("click button!");
+    }
+    
+}
