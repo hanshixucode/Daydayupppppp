89f60ecfee102328ca38c84ec86921b0a7564d47
hanshixucode <13569270593@163.com>
2021-09-20 22:13:40 +0800

    提交配置

---
+++ LoopScrollbar/Assets/Scripts/LoopManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoopManager : MonoBehaviour
+{
+    public List<RectTransform> iconlist = new List<RectTransform>();
+
+    public int IconNumber;
+    
+    [SerializeField]
+    private ScrollRect _ScrollRect;
+    [SerializeField]
+    private Scrollbar _Scrollbar;
+
+    [SerializeField] 
+    private RectTransform ContentRect;
+
+
+    private void Awake()
+    {
+        InitScrollrect();
+    }
+
+    private void OnEnable()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDestroy()
+    {
+        throw new NotImplementedException();
+    }
+    /// <summary>
+    /// 初始化数据
+    /// </summary>
+    private void InitScrollrect()
+    {
+        IconNumber = 100;
+        _ScrollRect = transform.GetComponent<ScrollRect>();
+        ContentRect = transform.GetChild(0).GetComponent<RectTransform>();
+        _Scrollbar = transform.parent.GetChild(1).GetComponent<Scrollbar>();
+    }
+    
+}
---
+++ LoopScrollbar/Assets/Scripts/ScrollBarManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollBarManager : MonoBehaviour
+{
+    [SerializeField]
+    private ScrollRect _ScrollRect;
+    [SerializeField]
+    private Scrollbar _Scrollbar;
+
+    [SerializeField] 
+    private RectTransform ContentRect;
+    
+    
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _ScrollRect = transform.GetComponent<ScrollRect>();
+        ContentRect = transform.GetChild(0).GetComponent<RectTransform>();
+        _Scrollbar = transform.parent.GetChild(1).GetComponent<Scrollbar>();
+        _Scrollbar.onValueChanged.AddListener(updatescrollbar);
+    }
+
+    private void OnEnable()
+    {
+        EventDispatch.Instance.AddEvent("Loop",CalculateIconValue);
+    }
+
+    private void OnDestroy()
+    {
+        EventDispatch.Instance.RemoveEvent("Loop",CalculateIconValue);
+    }
+
+    /// <summary>
+    /// 计算每个icon占用滑动条的value
+    /// </summary>
+    private void CalculateIconValue()
+    {
+        
+    }
+
+    void updatescrollbar(float data)
+    {
+        Debug.Log(data.ToString());
+    }
+    
+    
+}
