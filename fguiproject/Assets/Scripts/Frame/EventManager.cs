ce08815d75b89180208448199a851fd217061993
hanshixucode <13569270593@163.com>
2021-09-25 11:42:54 +0800

    fame

---
+++ fguiproject/Assets/Scripts/Frame/EventManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDispatch : Single<EventDispatch>
+{
+    //事件分发器
+    public Dictionary<string, Delegate> _event = null;
+
+    protected Dictionary<string, Delegate> Events
+    {
+        get
+        {
+            if (_event == null)
+                _event = new Dictionary<string, Delegate>();
+            return _event;
+        }
+    }
+
+    public void AddEvent(string name,Action ac)
+    {
+        Delegate Ac;
+        if (Events.TryGetValue(name, out Ac))
+        {
+            
+        }
+        else
+        {
+            Events.Add(name,ac);
+        }
+    }
+    
+    public void RemoveEvent(string name,Action ac)
+    {
+        if(_event == null) return;
+        Delegate Ac;
+        if (_event.TryGetValue(name, out Ac))
+        {
+            _event.Remove(name);
+        }
+    }
+    
+    public void TriggerEvent(string name)
+    {
+        if(_event == null) return;
+        Delegate Ac;
+        if (_event.TryGetValue(name, out Ac))
+        {
+            Delegate[] aclist = Ac.GetInvocationList();
+            for (int i = 0; i < aclist.Length; i++)
+            {
+                Action _ac = aclist[i] as Action;
+                if (_ac != null)
+                    _ac();
+            }
+            
+        }
+    }
+}
---
+++ fguiproject/Assets/Scripts/Frame/Single.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Single<T> where T: class,new()
+{
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+            }
+
+            return _instance;
+        }
+    }
+}
+
+public class SingleMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+    //Monobehavior singleon
+}
