d4ea5d9a1fb2ebd342de6331cb2fed4e34ff66e2
hanshixucode <13569270593@163.com>
2022-08-18 21:19:39 +0800

    异步

---
+++ NewStudy/Assets/Scripts/Promise/PromiseTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromiseTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //todo hsx promise Asyn
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
