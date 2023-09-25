92a4ab4b0ce7bd8eeeffb9d4e5359e6361580bfd
叶落云沉 <13569270593@163.com>
2023-09-25 21:21:11 +0800

    加班

--- AlgorithmWithLeetCode/YeluoFunc/YeluoFunc/Program230925.cs
+++ AlgorithmWithLeetCode/YeluoFunc/YeluoFunc/Program230925.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace YeluoFunc
 {
@@ -8,6 +10,8 @@ namespace YeluoFunc
         {
             //Console.WriteLine(TwoSum(new[] { 2, 7, 11, 15 }, 9).ToString());
             IsPalindrome(121);
+            Console.WriteLine(RomanToInt("IV").ToString());
+            LongestCommonPrefix(new[] { "ab", "a" });
         }
         
         /// <summary>
@@ -49,6 +53,63 @@ namespace YeluoFunc
                     return false;
             }
             return true;
+        }
+        /// <summary>
+        /// 13. 罗马数字转整数
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int RomanToInt(string s)
+        {
+            Dictionary<string, int> StringToNum = new Dictionary<string, int>()
+            {
+                {"I",1},
+                {"V",5},
+                {"X",10},
+                {"L",50},
+                {"C",100},
+                {"D",500},
+                {"M",1000},
+            };
+            int length = s.Length;
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                int value = StringToNum[s[i].ToString()];
+                if (i + 1 < length)
+                {
+                    if (value < StringToNum[s[i + 1].ToString()])
+                    {
+                        value *= -1;
+                    }
+                }
+                sum += value;
+            }
+            return sum;
+        }
+        
+        /// <summary>
+        /// 14. 最长公共前缀
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Contains("")) return "";
+            if (strs.Length == 1) return strs[0];
+            string sum = "";
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                var common = strs[0][i].ToString();
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if ( i > (strs[j].Length -1) || strs[j][i].ToString() != common)
+                        return sum;
+                }
+                sum += common;
+            }
+            
+            return sum;
         }
     }
 }
