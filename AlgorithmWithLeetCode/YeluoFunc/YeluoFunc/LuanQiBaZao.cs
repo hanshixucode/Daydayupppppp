e4b388bd8cc04ebf65bd0f949252ea966b598c28
hanshixucode <13569270593@163.com>
2023-10-15 22:45:00 +0800

    ?

--- AlgorithmWithLeetCode/YeluoFunc/YeluoFunc/LuanQiBaZao.cs
+++ AlgorithmWithLeetCode/YeluoFunc/YeluoFunc/LuanQiBaZao.cs
@@ -3,9 +3,32 @@ using System.Collections.Generic;
 
 namespace YeluoFunc
 {
+    //为做基类而生的抽象类与开闭原则
     public class LuanQiBaZao
     {
         public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    interface Icar
+    {
+        void Run();
+        void Stop();
+    }
+    internal abstract class Car : Icar
+    {
+        public void Stop()
+        {
+            
+        }
+        public abstract void Run();
+    }
+
+    internal class bike : Car
+    {
+        public override void Run()
         {
             
         }
