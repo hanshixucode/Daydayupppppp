61b5cdf74a74b56e1be51055299785064025bfe9
叶落云沉 <13569270593@163.com>
2023-10-19 16:13:15 +0800

    core inject

---
+++ AlgorithmWithLeetCode/YeluoFunc/CoreProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YeluoFunc
+{
+    //依赖注入
+    public class LuanQiBaZao
+    {
+        public static void Main(string[] args)
+        {
+            var sc = new ServiceCollection();
+            sc.AddScoped<ICar, Gti>();
+            var sp = sc.BuildServiceProvider();
+
+            ICar car = sp.GetService<ICar>();
+            car.Run();
+        }
+    }
+
+    interface ICar
+    {
+        void Run();
+    }
+
+    public class Gti : ICar
+    {
+        public void Run()
+        {
+            Console.WriteLine("weng weng weng");
+        }
+    }
+}
