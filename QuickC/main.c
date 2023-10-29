60e80ecea7fbcb0e82e647979f9c5710f4dc4e84
home <13569270593@163.com>
2023-10-29 21:09:13 +0800

    c

--- QuickC/main.c
+++ QuickC/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
+
     printf("Ok, One Week\n");
     return 0;
 }
+
+void GetName()
+{
+    printf("name");
+}
+
+struct Student
+{
+    int id;
+    string name;
+};
