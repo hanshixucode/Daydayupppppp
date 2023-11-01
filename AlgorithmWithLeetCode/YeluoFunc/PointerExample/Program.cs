d6cf8831fdb2fae586aa9e333cb3ca9ef39a544b
叶落云沉 <13569270593@163.com>
2023-11-01 15:41:15 +0800

    指针到此为止

--- AlgorithmWithLeetCode/YeluoFunc/PointerExample/Program.cs
+++ AlgorithmWithLeetCode/YeluoFunc/PointerExample/Program.cs
@@ -2,12 +2,49 @@
 using static System.Console;
 
 namespace PointerPlayground
-{
-    public class Program
+{ 
+    public unsafe class Program
     {
-        unsafe public static void Main()
+        public unsafe static void Main()
         {
+            int x = 10;
+            short y = -1;
+            byte y2 = 4;
+            double z = 1.5;
+            int* pX = &x;
+            short* pY = &y;
+            double* pZ = &z;
+            *pX = 20;
             
+            //栈数组
+            decimal* pDecimals = stackalloc decimal[10];
+            double* pDoubles = stackalloc double[20];
+            *pDoubles = 3.0;
+            *(pDoubles + 1) = 4.0;
+            pDoubles[0] = 3.0;
+            pDoubles[1] = 4.0;
+            Console.WriteLine($"{pDoubles[0]} + {pDoubles[50]}");
+            var q = new Quick();
+            q.QuickArray();
         }
     }
+
+    internal class Quick
+    {
+        public unsafe void QuickArray()
+        {
+            string userInput = ReadLine();
+            uint size = uint.Parse(userInput);
+
+            long* pArray = stackalloc long[(int)size];
+            for (int i = 0; i < size; i++)
+            {
+                pArray[i] = i * i;
+                WriteLine($"{i} = {*(pArray + i)}");
+            }
+
+            ReadLine();
+        }
+    }
+    
 } 
