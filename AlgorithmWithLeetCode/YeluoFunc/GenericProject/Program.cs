42bdfea604de36d347e957dbe931180021fc5fe6
叶落云沉 <13569270593@163.com>
2023-11-01 18:52:11 +0800

    ...

---
+++ AlgorithmWithLeetCode/YeluoFunc/GenericProject/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Generic
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; private set; }
+        public LinkedListNode<T> Next { get; internal set; }
+        public LinkedListNode<T> Prev { get; internal set; }
+    }
+
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public LinkedListNode<T> First { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
+
+        public LinkedListNode<T> AddLast(T node)
+        {
+            var newNode = new LinkedListNode<T>(node);
+            if (First != null)
+            {
+                First = newNode;
+                Last = First;
+            }
+            else
+            {
+                LinkedListNode<T> previous = Last;
+                Last.Next = newNode;
+                Last = newNode;
+                Last.Prev = previous;
+            }
+
+            return newNode;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = First;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
