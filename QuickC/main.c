c422b88df5748d08e8a1a3ecd3ded46f126c4cec
home <13569270593@163.com>
2023-11-04 17:35:49 +0800

    c

--- QuickC/main.c
+++ QuickC/main.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
+void GetName();
+
 int main() {
 
+    const int AMOUNT = 100;
     printf("Ok, One Week\n");
+    int a = 0;
+    int b = 0;
+    scanf("%d",&a);
+    scanf("%d", &b);
+    printf("%d",AMOUNT + a + b);
+    printf(" a++%d",a++);
+    printf(" a%d",a);
+    GetName();
     return 0;
 }
 
 void GetName()
 {
-    printf("name");
+    printf("ssss");
 }
 
 struct Student
 {
     int id;
-    string name;
 };
