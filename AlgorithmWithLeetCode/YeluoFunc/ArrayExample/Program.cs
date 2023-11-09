3b5873f8d2583609c66d8576d8026f00d62d4c3d
叶落云沉 <13569270593@163.com>
2023-11-09 17:20:00 +0800

    array

---
+++ AlgorithmWithLeetCode/YeluoFunc/ArrayExample/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //二维数组 很少用
+            // int[,] array2d = new int[3, 3];
+            int[,] array2d =
+            {
+                {1,2,3},
+                {1,2,3},
+                {1,2,3}
+            }
+            ;
+            //锯齿数组
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[2] { 1, 2 };
+            jagged[1] = new int[6] { 1, 2,1,2,1,2 };
+            jagged[2] = new int[3] { 1, 2,3 };
+            
+            
+            //Array
+            var intarray = Array.CreateInstance(typeof(int), 5);
+            for (int i = 0; i < intarray.Length; i++)
+            {
+                intarray.SetValue(33, i);
+            }
+
+            for (int i = 0; i < intarray.Length; i++)
+            {
+                Console.WriteLine(intarray.GetValue(i));
+            }
+
+            int[] array2 = (int[])intarray;
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine(array2[i]);
+            }
+            
+            var changearray = intarray.Clone() as Array;
+            changearray.SetValue(55,4);
+            changearray.SetValue(55,0);
+            Array.Copy(changearray, intarray, intarray.Length - 2);
+
+            Sort ss = new Sort();
+            ss.SortTest();
+        }
+    }
+
+    public class Sort
+    {
+        public void SortTest()
+        {
+            Person[] persons =
+            {
+                new Person() { FirstName = "c", LastName = "1" },
+                new Person() { FirstName = "b", LastName = "1" },
+                new Person() { FirstName = "a", LastName = "2" },
+            };
+            Array.Sort(persons);
+            foreach (Person person in persons)
+            {
+                Console.WriteLine($"one {person.FirstName}, two {person.LastName}");
+            }
+        }
+    }
+
+    public class Person : IComparable<Person>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CompareTo(Person? other)
+        {
+            if (other == null) return 1;
+            int result = string.Compare(this.LastName, other.LastName);
+            if (result == 0)
+            {
+                result = String.Compare(this.FirstName, other.FirstName);
+            }
+
+            return result;
+        }
+    }
+
+}
