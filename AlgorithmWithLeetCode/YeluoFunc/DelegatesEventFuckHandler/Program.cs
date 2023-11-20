8d7b57c8a058ec8cbe34c17fc34c13167c854b34
叶落云沉 <13569270593@163.com>
2023-11-20 19:04:46 +0800

    bubble

---
+++ AlgorithmWithLeetCode/YeluoFunc/DelegatesEventFuckHandler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Linq;
+
+namespace FuckC
+{
+    internal class MainClass
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+
+    internal class BubbleSorter
+    {
+        delegate void IntInvoker(int x);
+
+        public void Sort<T>(IList<T> sortArray, Func<T, T, bool> comparison)
+        {
+            bool isSwapped = true;
+            do
+            {
+                isSwapped = false;
+                for (int i = 0; i < sortArray.Count; i++)
+                {
+                    if (comparison(sortArray[i], sortArray[i + 1]))
+                    {
+                        var temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        isSwapped = true;
+                    }
+                }
+                
+            } while (isSwapped);
+        }
+    }
+}
