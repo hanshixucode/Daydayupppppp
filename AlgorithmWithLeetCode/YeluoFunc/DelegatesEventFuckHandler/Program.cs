b8a2425bcd61b18b28900ffc12b37541b0588412
叶落云沉 <13569270593@163.com>
2023-11-21 14:51:51 +0800

    闭包

--- AlgorithmWithLeetCode/YeluoFunc/DelegatesEventFuckHandler/Program.cs
+++ AlgorithmWithLeetCode/YeluoFunc/DelegatesEventFuckHandler/Program.cs
@@ -12,7 +12,24 @@ namespace FuckC
     {
         public static void Main()
         {
+            List<int> array = new List<int>()
+            {
+                1, 2, 8, 2, 5
+            };
+            BubbleSorter.Sort(array, delegate(int a, int b)
+            {
+                return a > b;
+            });
+            foreach (var a in array)
+            {
+                Console.WriteLine(a);
+            }
             
+            //闭包 委托内部调用外部变量
+            int ss = 5;
+            Func<int, int> f = i => i + ss;
+            ss = 7;
+            Console.WriteLine(f(3));
         }
     }
 
@@ -20,13 +37,13 @@ namespace FuckC
     {
         delegate void IntInvoker(int x);
 
-        public void Sort<T>(IList<T> sortArray, Func<T, T, bool> comparison)
+        public static void Sort<T>(IList<T> sortArray, Func<T, T, bool> comparison)
         {
             bool isSwapped = true;
             do
             {
                 isSwapped = false;
-                for (int i = 0; i < sortArray.Count; i++)
+                for (int i = 0; i < sortArray.Count - 1; i++)
                 {
                     if (comparison(sortArray[i], sortArray[i + 1]))
                     {
