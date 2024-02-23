53090a650e1d7f3b29014e918fd78ae7b170e47e
home <13569270593@163.com>
2024-02-23 16:55:07 +0800

    一些更新

---
+++ MVCorMVPorMVVM/Assets/Scripts/SetupView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupView
+{
+    public InputField nameInput;
+    public Text nameText;
+    public InputField jobInput;
+    public Text jobText;
+    public InputField atkInput;
+    public Text atkText;
+
+    public Slider SuccessSlider;
+    public Text SuccessSliderText;
+
+    public Toggle JoinToggle;
+    public Button joinbtn;
+    public Button waitbtn;
+}
