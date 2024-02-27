298cdd2c8a60443a6e42aa3997308e13e7d3a0a0
叶落云沉 <13569270593@163.com>
2024-02-27 18:54:04 +0800

    uiview 生命周期

--- MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/UnityGuiView.cs
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/UnityGuiView.cs
@@ -5,6 +5,14 @@ namespace MVVM
     public interface IView<T> where T : ViewModelBase
     { 
         T BindeingContext { get; set; }
+        void OnActive();
+        void OnShow();
+        void OnShowed();
+        void OnHide();
+        void OnHidden();
+        void OnDisappear();
+        void OnDestory();
+        
     }
     public class UnityGuiView<T>: MonoBehaviour,IView<T> where T : ViewModelBase
     {
@@ -27,7 +35,70 @@ namespace MVVM
                 ViewModelProperty.Value = value;
             }
         }
-        
+
+        /// <summary>
+        /// 激活View
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnActive()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 显示View
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnShow()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 显示后
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnShowed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 隐藏前
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnHide()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 隐藏后
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnHidden()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 隐藏完成False
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnDisappear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 销毁
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnDestory()
+        {
+            throw new System.NotImplementedException();
+        }
+
         protected virtual void OnBindingContextChanged(T oldViewModel, T newViewModel)
         {
             Binder.Unbind(oldViewModel);
