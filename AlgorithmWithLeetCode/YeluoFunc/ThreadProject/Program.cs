98832fd6513d8172bd3b6fac5dab9d2902552593
home <13569270593@163.com>
2024-03-03 18:11:11 +0800

    多线程 异步相关语法补充

---
+++ AlgorithmWithLeetCode/YeluoFunc/ThreadProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var th = new Thread((() =>
+{
+    for (int i = 0; i < 20; i++)
+    {
+        Thread.Sleep(200);
+        Console.WriteLine("Hello, World!");
+    }
+}));
+
+th.Start();
+Console.WriteLine("???");
+th.Join();
+Console.WriteLine("Done");
