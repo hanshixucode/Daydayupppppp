89314dd8e93a6fdd31ce53742323eae2af8d3e3d
叶落云沉 <13569270593@163.com>
2024-03-05 17:55:10 +0800

    线程安全相关的一些学习，包括原生的方法

--- AlgorithmWithLeetCode/YeluoFunc/ThreadProject/Program.cs
+++ AlgorithmWithLeetCode/YeluoFunc/ThreadProject/Program.cs
@@ -1,32 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var th = new Thread((() =>
+// var th = new Thread((() =>
+// {
+//     try
+//     {
+//         while (true)
+//         {
+//             Thread.Sleep(1);
+//         }
+//         // for (int i = 0; i < 20; i++)
+//         // {
+//         //     Thread.Sleep(200);
+//         //     Console.WriteLine("Hello, World!");
+//         // }
+//     }
+//     catch(ThreadInterruptedException)
+//     {
+//         Console.WriteLine("thread over");
+//     }
+//     finally
+//     {
+//         Console.WriteLine("Finish");
+//     }
+// }));
+// th.Start();
+// Console.WriteLine("???");
+// Thread.Sleep(1000);
+// th.Interrupt();
+// th.Join();
+// Console.WriteLine("Done");
+
+using System.Collections.Concurrent;
+
+var queue = new ConcurrentQueue<int>();
+
+var producer = new Thread(AddNumber);
+var consumer1 = new Thread(ReadNumber);
+var consumer2 = new Thread(ReadNumber);
+
+producer.Start();
+consumer1.Start();
+consumer2.Start();
+
+producer.Join();
+consumer1.Interrupt();
+consumer2.Interrupt();
+consumer1.Join();
+consumer2.Join();
+
+
+void AddNumber()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        Thread.Sleep(20);
+        queue.Enqueue(i);
+    }
+}
+
+void ReadNumber()
 {
     try
     {
         while (true)
         {
+            if (queue.TryDequeue(out var result))
+            {
+                Console.WriteLine(result);
+            }
             Thread.Sleep(1);
         }
-        // for (int i = 0; i < 20; i++)
-        // {
-        //     Thread.Sleep(200);
-        //     Console.WriteLine("Hello, World!");
-        // }
     }
-    catch(ThreadInterruptedException)
+    catch (ThreadInterruptedException)
     {
-        Console.WriteLine("thread over");
+        Console.WriteLine("interrupted");
     }
-    finally
-    {
-        Console.WriteLine("Finish");
-    }
-}));
-
-th.Start();
-Console.WriteLine("???");
-Thread.Sleep(1000);
-th.Interrupt();
-th.Join();
-Console.WriteLine("Done");
+}
