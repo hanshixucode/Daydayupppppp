d7dfebfb66f132c2e7be899b2c38892c42498b9b
叶落云沉 <13569270593@163.com>
2024-03-06 15:03:02 +0800

    异步与接口

--- AlgorithmWithLeetCode/YeluoFunc/ThreadProject/TaskProject.cs
+++ AlgorithmWithLeetCode/YeluoFunc/ThreadProject/TaskProject.cs
@@ -1,29 +1,52 @@
 ﻿namespace ThreadProject;
 
+public interface IFoo
+{
+    Task FooAsync();
+}
 public class TaskProject
 {
     public static void Main()
     {
-        var temp = new Template();
-        temp.TaskRun();
+        var test = new Test<Template>(new Template());
+        test.TestAsync();
         Console.ReadKey();
     }
 }
 
-public class Template
+public class Test<T> where T : IFoo
+{
+    public T _foo { get; private set; }
+    public Test(T foo)
+    {
+        this._foo = foo;
+    }
+
+    public async Task TestAsync()
+    {
+        await _foo.FooAsync();
+    }
+}
+
+public class Template : IFoo
 {
     public void TaskRun()
     {
-        Main();
+        MainAsync();
     }
-    public async Task Main()
+
+    public Task TestTask()
+    {
+        return null;
+    }
+    public async Task MainAsync()
     {
         Console.WriteLine("1");
         await FooAsync();
         Console.WriteLine("2");
     }
 
-    async Task FooAsync()
+    public async Task FooAsync()
     {
         Console.WriteLine("3");
         await Task.Delay(2000);
