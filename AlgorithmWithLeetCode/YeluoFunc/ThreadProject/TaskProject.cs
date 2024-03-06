55fa546442c2d4444a4e06b421db0823a578224b
叶落云沉 <13569270593@163.com>
2024-03-06 10:47:36 +0800

    task用法

--- AlgorithmWithLeetCode/YeluoFunc/ThreadProject/TaskProject.cs
+++ AlgorithmWithLeetCode/YeluoFunc/ThreadProject/TaskProject.cs
@@ -5,23 +5,28 @@ public class TaskProject
     public static void Main()
     {
         var temp = new Template();
-        temp.Main();
+        temp.TaskRun();
+        Console.ReadKey();
     }
 }
 
 public class Template
 {
+    public void TaskRun()
+    {
+        Main();
+    }
     public async Task Main()
     {
         Console.WriteLine("1");
         await FooAsync();
-        Console.WriteLine("4");
+        Console.WriteLine("2");
     }
 
     async Task FooAsync()
     {
-        Console.WriteLine("2");
-        await Task.Delay(10);
         Console.WriteLine("3");
+        await Task.Delay(2000);
+        Console.WriteLine("4");
     }
 }
