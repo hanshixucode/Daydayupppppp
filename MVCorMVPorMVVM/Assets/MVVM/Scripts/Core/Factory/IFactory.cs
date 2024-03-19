a9e04985709ff6eb06bd02b7573fd31e2dea8426
叶落云沉 <13569270593@163.com>
2024-03-19 17:30:16 +0800

    工厂模式接口

---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Factory/IFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVVM.Factory
+{
+    public interface IFactory
+    {
+        object AcquireObject(string className);
+        object AcquireObject(Type type);
+        object AcquireObject<IIstance>() where IIstance : class, new();
+        void ReleaseObject(object obj);
+    }
+}
