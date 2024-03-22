9c7d6fd4a25ab6cde461063d016c1ae29dddfc05
叶落云沉 <13569270593@163.com>
2024-03-22 11:29:50 +0800

    对象池工厂

---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Factory/PoolFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MVVM.Factory
+{
+    public class PoolFactory : IFactory
+    {
+        public object AcquireObject(string className)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object AcquireObject(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object AcquireObject<IIstance>() where IIstance : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReleaseObject(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
