a24d8fb1485bb82fc5abb5a367f3959a1aeba354
home <13569270593@163.com>
2024-03-25 23:11:48 +0800

    单例工厂

--- MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Factory/SingleFactory.cs
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Factory/SingleFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using MVVM.Helper;
 
 namespace MVVM.Factory
 {
@@ -8,19 +10,58 @@ namespace MVVM.Factory
     /// </summary>
     public class SingleFactory : IFactory
     {
+        /// <summary>
+        /// 唯一字典,避免多个SingleFactory同时访问
+        /// </summary>
+        private static Dictionary<Type, object> cachedObjects = null;
+        //只读 为了线程安全 避免线程锁对象改变
+        private static readonly object _lock = new object();
+
+        private Dictionary<Type, object> CachedObjects
+        {
+            get
+            {
+                //lock(this) 只对当前的对象有效果
+                lock (_lock)
+                {
+                    if (cachedObjects == null)
+                        cachedObjects = new Dictionary<Type, object>();
+                    return cachedObjects;
+                }
+            }
+        }
         public object AcquireObject(string className)
         {
-            throw new NotImplementedException();
+            return AcquireObject(FactoryHelper.ResolveType(className));
         }
 
         public object AcquireObject(Type type)
         {
-            throw new NotImplementedException();
+            if (CachedObjects.ContainsKey(type))
+            {
+                return CachedObjects[type];
+            }
+
+            lock (_lock)
+            {
+                CachedObjects.Add(type, Activator.CreateInstance(type,false));
+                return CachedObjects[type];
+            }
         }
 
         public object AcquireObject<IIstance>() where IIstance : class, new()
         {
-            throw new NotImplementedException();
+            var type = typeof(IIstance);
+            if (CachedObjects.ContainsKey(type))
+            {
+                return CachedObjects[type];
+            }
+            
+            lock (_lock)
+            {
+                CachedObjects.Add(type, Activator.CreateInstance(type, new IIstance()));
+                return CachedObjects[type];
+            }
         }
 
         public void ReleaseObject(object obj)
