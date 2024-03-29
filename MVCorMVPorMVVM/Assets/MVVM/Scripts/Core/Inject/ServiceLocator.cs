c4640fbd9b372b7e5efdda8cb893bb37fc07a066
叶落云沉 <13569270593@163.com>
2024-03-29 18:34:01 +0800

    周日回来补充

---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Inject/ServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MVVM.Factory;
+
+namespace MVVM.Inject
+{
+    public class ServiceLocator
+    {
+        private static SingleFactory _singleFactory = new SingleFactory();
+        private static TransientFactory _transientFactory = new TransientFactory();
+
+        private static readonly Dictionary<Type, Func<object>> container = new Dictionary<Type, Func<object>>();
+        
+        
+    }
+}
