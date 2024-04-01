b13d31ed38a0fbe377e1ebd14de3e98f9d52d35f
叶落云沉 <13569270593@163.com>
2024-04-01 23:11:17 +0800

    。。。不知道改动了什么

--- MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Repository/IRepository.cs
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Repository/IRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace MVVM.Repository
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVVM.Repository
 {
-    public interface IRepository
+    /// <summary>
+    /// 仓储模式的接口
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T : class, new()
     {
-        
+        void Insert(T instance);
+        void Delete(T instance);
+        void Update(T instance);
+        IEnumerable<T> Select(Func<T, bool> func);
     }
 }
