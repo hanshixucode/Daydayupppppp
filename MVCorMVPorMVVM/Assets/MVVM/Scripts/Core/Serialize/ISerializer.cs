a961d383e4fb2f42522d01d3f64278321a787558
叶落云沉 <13569270593@163.com>
2024-04-02 11:20:09 +0800

    序列化接口

---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Serialize/ISerializer.cs
@@ -0,0 +1,13 @@
+﻿namespace MVVM.Serialize
+{
+    /// <summary>
+    /// 序列化接口
+    /// </summary>
+    public interface ISerializer
+    {
+        //序列化
+        string Serialize<T>(T obj, bool readableOutput = false) where T : class, new();
+        //反序列化
+        T Deserialize<T>(string json) where T : class, new();
+    }
+}
---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Serialize/SerializeJson.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MVVM.Serialize
+{
+    public class SerializeJson : ISerializer
+    {
+        public string Serialize<T>(T obj, bool readableOutput = false) where T : class, new()
+        {
+            //JsonUtility.ToJson(obj) something....
+            throw new System.NotImplementedException();
+        }
+
+        public T Deserialize<T>(string json) where T : class, new()
+        {
+            return JsonUtility.FromJson<T>(json);
+        }
+    }
+}
