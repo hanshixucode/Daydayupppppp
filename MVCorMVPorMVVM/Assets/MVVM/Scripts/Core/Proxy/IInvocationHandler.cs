74d6fa643d9ebde2c3c4a252ee839358a6146c47
叶落云沉 <13569270593@163.com>
2024-04-02 18:44:28 +0800

    aop proxy

---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Proxy/IInvocationHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace MVVM.Proxy
+{
+    public interface IInvocationHandler
+    {
+        void PreProcess();
+        object Invoke(object proxy, MethodInfo methodInfo, object[] args);
+        void PostProcess();
+    }
+}
---
+++ MVCorMVPorMVVM/Assets/MVVM/Scripts/Core/Proxy/LogInvocationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using MVVM.Log;
+
+namespace MVVM.Proxy
+{
+    public class LogInvocationHandler : IInvocationHandler
+    {
+        public void PreProcess()
+        {
+            LogFactory.Instacne.Resolve<ConsoleLogStrategy>().Log("pre");
+        }
+
+        public object Invoke(object proxy, MethodInfo methodInfo, object[] args)
+        {
+            PreProcess();
+            var result = methodInfo.Invoke(proxy, args);
+            PostProcess();
+            return result;
+        }
+
+        public void PostProcess()
+        {
+            LogFactory.Instacne.Resolve<ConsoleLogStrategy>().Log("post");
+        }
+    }
+}
