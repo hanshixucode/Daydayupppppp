6a039337bbb27b086dcfa42e52139ee7e09b6cbd
叶落云沉 <13569270593@163.com>
2024-04-09 15:34:39 +0800

    移除swagger

---
+++ ServerTestProject/CommonLib/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonLib;
+
+public class Player
+{
+    public int Id { get; set; }
+    public int Level { get; set; }
+    public float Health { get; set; }
+}
---
+++ ServerTestProject/GameServer/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CommonLib;
+namespace GameServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GameController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public Player Get([FromRoute] int id)
+    {
+        var player = new Player(){Id = id};
+        return player;
+    }
+
+    [HttpPost("[action]")]
+    public Player Post(Player player)
+    {
+        return player;
+    }
+}
