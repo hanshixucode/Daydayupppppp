d7b56eb82e7fecd894cdb8145c9282b90f3b819c
叶落云沉 <13569270593@163.com>
2024-04-09 18:29:39 +0800

    依赖注入测试

--- ServerTestProject/GameServer/Controllers/GameController.cs
+++ ServerTestProject/GameServer/Controllers/GameController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CommonLib;
+using GameServer.Services;
+
 namespace GameServer.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class GameController : ControllerBase
 {
+    private readonly PlayerService _playerService;
+
+    public GameController(PlayerService playerService)
+    {
+        _playerService = playerService;
+    }
     [HttpGet("{id}")]
     public Player Get([FromRoute] int id)
     {
         var player = new Player(){Id = id};
+        _playerService.DoSomething();
         return player;
     }
 
---
+++ ServerTestProject/GameServer/Services/PlayerService.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Services;
+
+public class PlayerService
+{
+    public void DoSomething()
+    {
+        Console.WriteLine("hey!");
+    }
+}
