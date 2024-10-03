5e9495ae8237e6e5a4954556ec11ded42abbb7ed
叶落云沉 <13569270593@163.com>
2024-10-03 19:37:20 +0800

    10.3.3

--- QuickLua/10.3.lua
+++ QuickLua/10.3.lua
@@ -2,3 +2,19 @@
 a = {}
 a["x"] = 10
 print(a["x"])
+b = a
+--a = nil --匿名
+print(b["x"])
+
+for i = 1,100 do
+    a[i] = i* 2 
+end
+
+a.x = 10
+a.y = 20
+
+a = {}
+a[2.1] = 10
+a[2.222] = 10
+print(a[2.2])
+print(a[2.11])
