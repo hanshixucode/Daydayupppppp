6c4b1cde71820eddf43e68f02867fae11ba2b73e
叶落云沉 <13569270593@163.com>
2024-10-05 10:28:27 +0800

    10.5

---
+++ QuickLua/10.5.lua
@@ -0,0 +1,20 @@
+--table else
+--表构造器
+days = {"1","2"}
+print(days[1])
+a = {x = 1, y = 2} -- => a = {} a.x = 1 a.y = 2
+days[3] = "asd"
+l = "ss"
+a[l] = "asd"
+print(a["l"])
+print(a[l])
+w = {x = 0,y = 0, label = "console"}
+w[1] = "ano"
+w[x] = nil
+
+testtable = {
+    x = 5,
+    "1",
+    {x = 1, y = 2},
+    {x = 2, y = 3}
+}
