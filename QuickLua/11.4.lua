d900006d2a972d3826940959dc8a6acf5dbf1178
叶落云沉 <13569270593@163.com>
2024-11-04 11:15:40 +0800

    11.4-2

--- QuickLua/11.4.lua
+++ QuickLua/11.4.lua
@@ -1,5 +1,43 @@
 --table else else
-t_114 = {10,20,30}
+local t_114 = {10,20,30}
 table.insert(t_114,1,111)
 --{111,10,20,30}
 
+
+table.move(t_114,1,#t_114,2)
+--print(#t_114)
+table.move(t_114,2,#t_114,1)
+--print(#t_114)
+--[[for index, value in ipairs(t_114) do
+    print(inde, value) -- nil
+end
+for key, value in pairs(t_114) do
+    print(t_114[key], value)
+end]]--
+
+--function
+function add(a)
+    local sum = 0
+    for i = 1, #a do
+        sum = sum + a[i]
+    end
+    return sum
+end
+--print(add(t_114))
+--function(a,b) -> fun(3,4,5) - remove 5
+
+--variadic
+function adddic (...)
+    local s = 0
+    for _, value in ipairs{...} do
+        s = s + value
+    end
+    return s
+end
+--print(adddic(1,2,3,4,5,5,5,5,5))
+
+function foo1 (...)
+    print("calling",...)
+end
+--select
+print(select(2,"1","2","3"))
