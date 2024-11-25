36386d530a022e5b919cda76f010ae2045c84df3
叶落云沉 <13569270593@163.com>
2024-11-25 10:58:03 +0800

    闭包了解一下

---
+++ QuickLua/11.25.lua
@@ -0,0 +1,16 @@
+--闭包
+--[[local function digitbutton(digit)
+    return Button{
+        label = tostring(digit),
+        action = function ()
+            add(digit)
+        end
+    }
+end]]--
+local oldSin = math.sin
+--math.sin = function (x)
+  --  return oldSin(x * (math.pi / 180))
+--end
+
+print(oldSin(100))
+print(math.sin(100))
