8f6442f8f39d9e1b788fda10093862264b264107
叶落云沉 <13569270593@163.com>
2024-11-26 11:50:06 +0800

    no message

---
+++ QuickLua/11.26.lua
@@ -0,0 +1,29 @@
+--数据结构
+--array
+local a = {}
+for i = 1,100 do
+    a[i] = 0
+end
+print(#a)
+--[[矩阵
+local mt = {}
+for i = 1, N do
+    local row = {}
+    mt[i] = row
+    for j = 1, M do
+        row[j] = 0
+    end
+end
+ 
+--链表
+local list = nil
+list = {next = list, value = v}
+local l = list
+while l do
+    l = l.next
+end
+]]--
+--序列化
+f = load("i = i + 1")
+i = 0
+f()
