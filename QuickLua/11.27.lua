1c62d255e06d12e116809cbdfe872b323eaeb516
叶落云沉 <13569270593@163.com>
2024-11-27 21:40:02 +0800

    迭代器

--- QuickLua/11.27.lua
+++ QuickLua/11.27.lua
@@ -1,9 +1,10 @@
---module
+--[[module
 local m = require "math"
 print(m.sin(3.14))
 local h = require "hello"
 print(h.fact(3))
 
+--[[
 local module = {}
 module.content = "ss"
 
@@ -11,3 +12,16 @@ function module.func1()
     print("ssssssss")
 end
 return module
+]]--
+--迭代器 闭包
+local function values(t)
+    local i = 0
+    return function ()
+        i = i + 1;
+        return t[i]
+    end
+end
+local t = {10,20,30}
+for element in values(t) do
+    print(element)
+end
