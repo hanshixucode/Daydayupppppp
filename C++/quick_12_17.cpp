0680c7397d9994fd0f5654653423e384860b7b77
叶落云沉 <13569270593@163.com>
2024-12-17 18:32:12 +0800

    更新忽略

---
+++ C++/quick_12_17.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Administrator on 2024/12/17.
+//
+#include "iostream"
+using namespace std;
+
+#define day 7 // 宏常量
+
+int main() {
+    const int week  = 4;
+    //week = 5; 修饰变量
+    cout << day << endl;
+    cout <<"hello world"<<endl;
+    return 0;
+}
