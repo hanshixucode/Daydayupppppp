18a8919fbd269ba39fa2c09b6bc01775f0cdd37b
叶落云沉 <13569270593@163.com>
2025-01-24 12:26:35 +0800

    push

--- ACEU3DTestProjectx/ACEU3DTestProjectx/Assets/Scripts/GameManager.cs
+++ ACEU3DTestProjectx/ACEU3DTestProjectx/Assets/Scripts/GameManager.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
